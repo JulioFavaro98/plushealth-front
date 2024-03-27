f1aecf3375dafcc5e376ee88954de1845674588c
Julio Favaro <juliofv66@gmail.com>
2024-03-27 17:35:24 -0300

    medicos-list criado

---
+++ src/app/components/medicos/medicos-list/medicos-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medicos-list',
+  templateUrl: './medicos-list.component.html',
+  styleUrls: ['./medicos-list.component.css']
+})
+export class MedicosListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
