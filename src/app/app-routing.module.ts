34e4758bbdb69110e0a92b83386912c82db485a7
Julio Favaro <juliofv66@gmail.com>
2024-05-16 10:17:43 -0300

    Rota novo medico configurada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { MedicosListComponent } from './components/medicos/medicos-list/medicos-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { MedicoCreateComponent } from './components/medicos/medico-create/medico-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: NavComponent, canActivate: [AuthGuard], children: [
     { path: 'home', component: HomeComponent},
-    { path: 'medicos', component: MedicosListComponent}
+
+    { path: 'medicos', component: MedicosListComponent},
+    { path: 'medicos/create', component: MedicoCreateComponent}
   ]}
 ];
 
