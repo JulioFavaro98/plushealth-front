a2e32a8ac0246ca9361793af5b3a757ec3db9669
Julio Favaro <juliofv66@gmail.com>
2024-06-06 10:02:08 -0300

    MEDICO-DELETE finalizado

--- src/app/components/medicos/medico-delete/medico-delete.component.ts
+++ src/app/components/medicos/medico-delete/medico-delete.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import Inputmask from 'inputmask';
+import { ToastrService } from 'ngx-toastr';
+import { Medico } from 'src/app/models/medico';
+import { MedicosService } from 'src/app/services/medicos.service';
 
 @Component({
   selector: 'app-medico-delete',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoDeleteComponent implements OnInit {
 
-  constructor() { }
+  medico: Medico = {
+    id: '',
+    nome: '',
+    crm: '',
+    especialidade: '',
+    email: '',
+    telefone: ''
+  }
+
+
+  constructor(
+    private elementRef: ElementRef,
+    private service: MedicosService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this.medico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
   }
+
+  findById(): void {
+    this.service.findById(this.medico.id).subscribe(resposta => {
+      this.medico = resposta;
+    });
+  }
+
+  delete(): void {
+      this.service.delete(this.medico.id).subscribe(() => {
+        this.toast.success('Médico excluído com sucesso!', 'Delete');
+        this.router.navigate(['medicos'])
+      }, ex => {
+        if(ex.error.errors) {
+          ex.error.errors.forEach(element => {
+            this.toast.error(element.message);
+          });
+        } else {
+          this.toast.error(ex.error.message);
+        }
+      });
+    }
+
+  ngAfterViewInit(): void {
+    Inputmask({ mask: '(99) 9999-9999' }).mask(this.elementRef.nativeElement.querySelector('#telefone'));
+  }
+
+
+
 
 }
