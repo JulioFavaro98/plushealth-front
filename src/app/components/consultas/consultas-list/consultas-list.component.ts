4b407676e9d07d9bdb10708f0c1007696f65128d
Julio Favaro <juliofv66@gmail.com>
2024-06-10 16:52:38 -0300

    Filtro e metodo findAll para CONSULTAS-LIST

--- src/app/components/consultas/consultas-list/consultas-list.component.ts
+++ src/app/components/consultas/consultas-list/consultas-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Consulta } from 'src/app/models/consulta';
+import { ConsultaService } from 'src/app/services/consulta.service';
 
 @Component({
   selector: 'app-consultas-list',
@@ -10,27 +11,48 @@ import { Consulta } from 'src/app/models/consulta';
 })
 export class ConsultasListComponent implements OnInit {
 
-  ELEMENT_DATA: Consulta[] = [
-    {
-      id:                        1,
-      dataConsulta:   "15/06/2024",
-      horaConsulta:        "10:00",
-      statusConsulta:  "ANDAMENTO",
-      medico:                    1,
-      paciente:                  5,
-      nomeMedico:       "Dr André",
-      nomePaciente: "Maria Eduarda"
-    }
-  ]
+  ELEMENT_DATA: Consulta[] = []
+  FILTERED_DATA: Consulta[] = []
 
   displayedColumns: string[] = ['id', 'dataConsulta', 'horaConsulta', 'statusConsulta', 'medico', 'paciente', 'acoes'];
   dataSource = new MatTableDataSource<Consulta>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private service: ConsultaService) { }
 
   ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(){
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Consulta>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    }) 
+  }
+
+  retornaStatus(status: any){
+    if (status == '0'){
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  orderByStatus(status: any): void{
+    let list: Consulta[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if (element.statusConsulta == status){
+        list.push(element);
+      }
+    })
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Consulta>(this.FILTERED_DATA);
+    this.dataSource.paginator = this.paginator;
   }
 
   applyFilter(event: Event) {
