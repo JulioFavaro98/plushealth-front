be133df0a9d84934b098c9f445a92ec602927d3b
Julio Favaro <juliofv66@gmail.com>
2024-06-10 09:42:00 -0300

    Interface consulta.ts criada

---
+++ src/app/models/consulta.ts
@@ -0,0 +1,10 @@
+export interface Consulta {
+    id?:            any;
+    dataConsulta:   string;
+    horaConsulta:   string;
+    statusConsulta: string;
+    medico:         any;
+    paciente:       any;
+    nomeMedico:     string;
+    nomePaciente:   string;
+}
