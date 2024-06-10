4022faebfb698de1be875b4726ba324e48e3deef
Julio Favaro <juliofv66@gmail.com>
2024-06-10 16:21:53 -0300

    HTML e CSS para componente CONSULTAS-LIST

--- src/app/components/consultas/consultas-list/consultas-list.component.ts
+++ src/app/components/consultas/consultas-list/consultas-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Consulta } from 'src/app/models/consulta';
 
 @Component({
   selector: 'app-consultas-list',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultasListComponent implements OnInit {
 
+  ELEMENT_DATA: Consulta[] = [
+    {
+      id:                        1,
+      dataConsulta:   "15/06/2024",
+      horaConsulta:        "10:00",
+      statusConsulta:  "ANDAMENTO",
+      medico:                    1,
+      paciente:                  5,
+      nomeMedico:       "Dr André",
+      nomePaciente: "Maria Eduarda"
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'dataConsulta', 'horaConsulta', 'statusConsulta', 'medico', 'paciente', 'acoes'];
+  dataSource = new MatTableDataSource<Consulta>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
