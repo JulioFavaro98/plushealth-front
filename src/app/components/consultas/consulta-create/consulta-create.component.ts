5a4a29e26e0a89cbd5fadefb49a4b27e3649f28f
Julio Favaro <juliofv66@gmail.com>
2024-06-12 20:31:45 -0300

    Implementando a validação do form CONSULTA-CREATE

--- src/app/components/consultas/consulta-create/consulta-create.component.ts
+++ src/app/components/consultas/consulta-create/consulta-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-create',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaCreateComponent implements OnInit {
 
+  dataConsulta: FormControl = new FormControl(null, [Validators.required])
+  horaConsulta: FormControl = new FormControl(null, [Validators.required])
+  statusConsulta: FormControl = new FormControl(null, [Validators.required])
+  medico: FormControl = new FormControl(null, [Validators.required])
+  paciente: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return  this.dataConsulta.valid &&
+            this.horaConsulta.valid &&
+            this.statusConsulta.valid &&
+            this.medico.valid && 
+            this.paciente.valid;
   }
 
 }
